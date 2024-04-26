a7d0f52343d37116fee7aa895b17d77a72af30c4
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2024-04-26 18:02:54 -0300

    feat: adds cors config

--- api/src/functions/graphql.ts
+++ api/src/functions/graphql.ts
@@ -1,5 +1,6 @@
 import { createGraphQLHandler } from '@redwoodjs/graphql-server'
 
+import directives from 'src/directives/**/*.{js,ts}'
 import sdls from 'src/graphql/**/*.sdl.{js,ts}'
 import services from 'src/services/**/*.{js,ts}'
 
@@ -7,8 +8,14 @@ import { db, logger } from 'src/lib'
 
 export const handler = createGraphQLHandler({
   loggerConfig: { logger, options: {} },
+  directives,
   sdls,
   services,
+  cors: {
+    origin: 'http://localhost:8910',
+    credentials: true,
+
+  },
   onException: () => {
     db.$disconnect()
   },
