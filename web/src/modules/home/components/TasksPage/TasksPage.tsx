75c37141875e420f0a24999443df93e9fbb55bb1
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2024-04-26 18:07:44 -0300

    feat: adds task search components

---
+++ web/src/modules/home/components/TasksPage/TasksPage.tsx
@@ -0,0 +1,7 @@
+import TasksCell from "@modules/home/components/TasksCell/TasksCell";
+
+const TasksPage = ({ status }) => {
+  return <TasksCell status={status} />;
+};
+
+export default TasksPage;
