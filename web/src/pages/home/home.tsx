cbae06bfef56e04b51b4110311d7a6006517b505
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2024-04-26 18:07:55 -0300

    feat: adds task search component

--- web/src/pages/home/home.tsx
+++ web/src/pages/home/home.tsx
@@ -1,12 +1,11 @@
-export default function Home() {
+import TasksPage from '@modules/home/components/TasksPage';
+
+
+export default function HomePage() {
   return (
     <div>
       <h1>Home</h1>
-      <p>Find me in ./web/src/pages/Home/Home.tsx</p>
+      <TasksPage  status="ALL" />
     </div>
   )
 }
-
-
-// yarn remove eslint eslint-config-prettier eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks eslint-plugin-react-refresh eslint-plugin-storybook postcss postcss-import postcss-nesting prettier prettier-plugin-tailwindcss
-// yarn add --dev eslint eslint-config-prettier eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks eslint-plugin-react-refresh eslint-plugin-storybook postcss postcss-import postcss-nesting prettier prettier-plugin-tailwindcss
