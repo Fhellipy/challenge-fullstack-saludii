13fef3e7317c018d505eaeb6a5e6901cb7afae98
fhellipy.santana <ffhellipyc.santana@gmail.com>
2024-04-29 00:37:32 -0300

    refactor: adds task page

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,14 +1,12 @@
-import Home from 'src/pages/HomePage/HomePage'
-import NotFoundPage from 'src/pages/NotFoundPage/NotFoundPage'
-
-import { Route, Router } from '@redwoodjs/router'
+import { Route, Router } from '@redwoodjs/router';
+import TasksPage from './pages/TasksPage/TasksPage';
 
 
 export default function Routes() {
   return (
     <Router>
       <Route notfound page={NotFoundPage} />
-      <Route path="/" page={Home} name="home" />
+      <Route path="/" page={TasksPage} name="tasks" />
     </Router>
   )
 }
