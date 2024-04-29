18c765f10a17482718bef412731b145c55c5e914
fhellipy.santana <ffhellipyc.santana@gmail.com>
2024-04-29 00:36:00 -0300

    feat: adds tailwind config

---
+++ web/tailwind.config.ts
@@ -0,0 +1,44 @@
+import { type Config } from 'tailwindcss'
+
+const config: Config = {
+  darkMode: ['class'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/**/**/*.{ts,tsx}',
+    './src/index.html',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      colors: {
+        border: 'hsl(var(--border))',
+
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
+
+module.exports = config
