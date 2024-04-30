23d75e343ed9fdbf46a901bc9975b94e16e545d7
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2024-04-30 18:02:30 -0300

    feat: adds gradient style

--- web/src/pages/TasksPage/TasksPage.tsx
+++ web/src/pages/TasksPage/TasksPage.tsx
@@ -5,7 +5,6 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-import { Editor } from 'src/components';
 import { TaskStatus } from 'types/graphql';
 
 export default function TasksPage() {
@@ -17,21 +16,17 @@ export default function TasksPage() {
 
 
   return (
-    <main className='bg-zinc-950 w-full h-screen m-auto p-10'>
-      <div className="h-full bg-zinc-800 w-full grid overflow-hidden grid-cols-[16rem_1fr] rounded-lg">
-       <aside className='bg-zinc-700 h-full border-r text-foreground p-4'>
-       </aside>
-
-       <section className='text-foreground p-4 gap-2 flex flex-col overflow-auto'>
-         <Box className="bg-foreground p-4 rounded-lg">
+    <main className='bg-gradient-to-tr from-emerald-500 via-cyan-700 to-blue-500 w-full h-screen m-auto p-10'>
+      <div className=" max-w-[85rem] w-full h-full flex flex-col p-6 gap-6 bg-muted rounded-lg mx-auto shadow-custom">
+         <Box className="p-4 rounded-lg mx-4 text-muted-foreground shadow-custom">
            <InputLabel>Filtrar por Status</InputLabel>
-           <FormControl fullWidth>
+           <FormControl fullWidth id='form-control'>
                <Select
                  id="demo-simple-select"
                  value={status}
                  label="Status"
                  onChange={handleStatusChange}
-                 >
+               >
                  <MenuItem value="ALL">Todos</MenuItem>
                  <MenuItem value="PENDING">Pendentes</MenuItem>
                  <MenuItem value="COMPLETED">Concluídos</MenuItem>
@@ -39,10 +34,7 @@ export default function TasksPage() {
            </FormControl>
          </Box>
 
-         <Editor />
-
          <TasksCell  status={status} />
-       </section>
      </div>
     </main>
   )
