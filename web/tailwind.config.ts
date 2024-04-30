5eba88619e092b905ffa4efc900cfff5ce25935a
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2024-04-30 18:01:20 -0300

    feat: adds new style variables

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -30,12 +30,23 @@ const config: Config = {
           DEFAULT: 'hsl(var(--destructive))',
           foreground: 'hsl(var(--destructive-foreground))',
         },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
+      boxShadow: {
+				custom: '0 6px 19px rgba(0, 0, 0, 0.2)',
+			},
     },
   },
   plugins: [require('@tailwindcss/typography')],
