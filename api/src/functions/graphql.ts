8a7a92956871aaf2296713b54d5678a0fdce96b1
fhellipy.santana <ffhellipyc.santana@gmail.com>
2024-05-01 02:11:50 -0300

    feat: adds socket

--- api/src/functions/graphql.ts
+++ api/src/functions/graphql.ts
@@ -1,10 +1,71 @@
 import { createGraphQLHandler } from '@redwoodjs/graphql-server'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 
 import directives from 'src/directives/**/*.{js,ts}'
 import sdls from 'src/graphql/**/*.sdl.{js,ts}'
 import services from 'src/services/**/*.{js,ts}'
 
 import { db, logger } from 'src/lib'
+import { createTask, deleteTask, tasks, updatePositionTasks, updateTask } from 'src/services'
+
+const server = createServer()
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+})
+
+io.on('connection', (socket) => {
+  console.log(`Socket connected: ${socket.id}`)
+
+  socket.on('getTasks', async (status) => {
+    const allTasks = await tasks({ status })
+    socket.emit('allTask', allTasks)
+  })
+
+  socket.on('createTask', async (task) => {
+    await createTask({ input: task.input })
+
+    const allTasks = await tasks({ status: 'ALL' })
+    socket.emit('allTask', allTasks)
+    socket.broadcast.emit('allTask', allTasks)
+  })
+
+  socket.on('updateTask', async (task) => {
+    await updateTask({ id: task.id, input: task.input })
+
+    const allTasks = await tasks({ status: 'ALL' })
+    socket.emit('allTask', allTasks)
+    socket.broadcast.emit('allTask', allTasks)
+  })
+
+  socket.on('updatePositionTasks', async (input) => {
+    await updatePositionTasks({ input })
+
+    const allTasks = await tasks({ status: 'ALL' })
+    socket.emit('allTask', allTasks)
+    socket.broadcast.emit('allTask', allTasks)
+  })
+
+  socket.on('deleteTask', async (id) => {
+    await deleteTask({ id })
+
+    const allTasks = await tasks({ status: 'ALL' })
+    socket.emit('allTask', allTasks)
+    socket.broadcast.emit('allTask', allTasks)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('Socket disconnected')
+  })
+})
+
+server.listen(3000, () => {
+  console.log('Socket server running on port 3000');
+});
+
 
 export const handler = createGraphQLHandler({
   loggerConfig: { logger, options: {} },
@@ -12,7 +73,7 @@ export const handler = createGraphQLHandler({
   sdls,
   services,
   cors: {
-    origin: 'http://localhost:8910',
+    origin: '*',
     credentials: true,
 
   },
@@ -20,3 +81,5 @@ export const handler = createGraphQLHandler({
     db.$disconnect()
   },
 })
+
+export default server
