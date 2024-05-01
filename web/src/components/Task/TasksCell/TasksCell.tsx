d5cbf546cdcaf7e77d97e31a747eddee3f44be33
fhellipy.santana <ffhellipyc.santana@gmail.com>
2024-05-01 02:14:45 -0300

    fix: adds socket

--- web/src/components/Task/TasksCell/TasksCell.tsx
+++ web/src/components/Task/TasksCell/TasksCell.tsx
@@ -1,8 +1,9 @@
-import { CellFailureProps, CellSuccessProps, TypedDocumentNode, useMutation } from '@redwoodjs/web';
+import { CellFailureProps, CellSuccessProps, TypedDocumentNode } from '@redwoodjs/web';
 import { CircleAlertIcon } from 'lucide-react';
-import type { TasksQuery, TasksQueryVariables } from 'types/graphql';
+import { useState } from 'react';
+import { socket } from 'src/lib';
+import type { CreateTaskInput, TasksQuery, TasksQueryVariables, UpdateTaskInput } from 'types/graphql';
 import { Sortable } from '../Sortable/Sortable';
-
 
 export const QUERY:TypedDocumentNode<
   TasksQuery,
@@ -21,48 +22,9 @@ export const QUERY:TypedDocumentNode<
   }
 `;
 
-const UPDATE_TASK = gql`
-  mutation updateTask(
-    $id: String!
-    $input: UpdateTaskInput!
-  ) {
-    updateTask(id: $id, input: $input) {
-      id
-    }
-  }
-`
-
-const UPDATE_POSITION_TASKS = gql`
-  mutation updatePositionTasks(
-    $input: UpdatePositionTasksInput!
-  ) {
-    updatePositionTasks(input: $input) {
-      id
-      taskIdPrev
-    }
-  }
-`
-
-const DELETE_TASK = gql`
-  mutation deleteTask($id: String!) {
-    deleteTask(id: $id) {
-      id
-    }
-  }
-`
-
-
 export const Loading = () => (
   <div className="z-50 flex h-full w-full flex-col items-center justify-center">
     <div  className='inline-block animate-spin rounded-full border-blue-600 border-b-transparent h-14 w-14 border-[5px]'/>
-  </div>
-)
-
-
-export const Empty = () => (
-  <div className='flex flex-col gap-2 items-center h-full justify-center text-orange-600'>
-    <CircleAlertIcon className='w-16 h-16' />
-    <div className='font-black'>Nenhuma tarefa encontrada</div>
   </div>
 )
 
@@ -74,40 +36,35 @@ export const Failure = ({ error }: CellFailureProps<TasksQueryVariables>) => (
 );
 
 export const Success = ({ tasks }: CellSuccessProps<TasksQuery, TasksQueryVariables>) => {
-  const [updateTask] = useMutation(UPDATE_TASK)
-  const [updatePositionTasks] = useMutation(UPDATE_POSITION_TASKS)
+  const [allTasks, setAllTasks] = useState(tasks)
 
-  const [deleteTask] = useMutation(DELETE_TASK)
+  const handleCreateTask = (input: CreateTaskInput) => {
+    socket.emit('createTask', { input })
+  }
 
-  const handleCheckboxChange = (id: string, status: string) => {
-    updateTask({
-      variables: {
-        id,
-        input: { status }
-      }
-    })
+  const handleUpdateTask = (id: string, input: UpdateTaskInput) => {
+    socket.emit('updateTask', { id, input })
   }
 
   const handleUpdatePositionTasks = (tasks: { id: string, taskIdPrev: string | null }[]) => {
-    updatePositionTasks({
-      variables: {
-        input: {
-          tasks
-        }
-      }
-    })
+     socket.emit('updatePositionTasks', tasks)
   }
 
   const handleDelete = (id: string) => {
-    deleteTask({ variables: { id } })
+    socket.emit('deleteTask', id)
   }
+
+  socket.on('allTask', allTasks => {
+    setAllTasks(allTasks)
+  })
 
   return (
     <Sortable
-      tasks={tasks}
-      handleCheckboxChange={handleCheckboxChange}
+      tasks={allTasks}
+      handleUpdateTask={handleUpdateTask}
       handleDelete={handleDelete}
       handleUpdatePositionTasks={handleUpdatePositionTasks}
+      handleCreateTask={handleCreateTask}
     />)
 };
 
