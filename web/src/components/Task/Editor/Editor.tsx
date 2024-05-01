313feffa4e28febdac3d5e76f91ef5acf42ded4d
fhellipy.santana <ffhellipyc.santana@gmail.com>
2024-05-01 02:13:48 -0300

    feat: adds content manipulation functions

--- web/src/components/Task/Editor/Editor.tsx
+++ web/src/components/Task/Editor/Editor.tsx
@@ -5,6 +5,7 @@ import Underline from "@tiptap/extension-underline"
 import { EditorContent, FloatingMenu, useEditor } from "@tiptap/react"
 import StarterKit from "@tiptap/starter-kit"
 import { useEffect, useMemo, useRef, useState } from "react"
+import { useDebounce } from "src/hooks"
 import { BubbleMenuComponent } from './BubbleMenuComponent'
 import "./editor.css"
 
@@ -26,13 +27,40 @@ const extensions = [
 
 type EditorProps = {
   initialContent: string;
+  onChange: (content: string) => void;
+  placeholder: string;
 }
 
-export function Editor({ initialContent } : EditorProps ) {
+export function Editor({ initialContent, placeholder, onChange } : EditorProps ) {
   const [editable, setEditable] = useState(false);
+  const [change, setChange] = useState(false);
   const editorRef = useRef(null);
 
+  const [content, setContent] = useState("");
+  const contentDebounce = useDebounce(setContent, 1500);
+
+  function removeHTMLTags(str: string) {
+    const checkboxComponent = str.includes('data-type="taskList"')
+
+    if(checkboxComponent) return str;
+
+    return str.replace(/<(?!img\s)[^>]+>/g, "");
+  }
+
+  useEffect(() => {
+    if(change) {
+      onChange(content);
+      setChange(false);
+    }
+  }, [content, onChange]);
+
   const handleClickOutside = (event) => {
+    const modalLink = document.getElementById("modal-link");
+
+    if (modalLink && modalLink.contains(event.target)) {
+      return;
+    }
+
     if (editorRef.current && !editorRef.current.contains(event.target)) {
       setEditable(false);
     }
@@ -49,6 +77,11 @@ export function Editor({ initialContent } : EditorProps ) {
     extensions,
     content: initialContent,
     editable,
+    onUpdate: ({ editor }) => {
+      const content = editor.getHTML()
+      contentDebounce(content);
+      setChange(true);
+    },
     editorProps: {
       attributes: {
         class: "outline-none",
@@ -57,28 +90,29 @@ export function Editor({ initialContent } : EditorProps ) {
     }
   })
 
+
   useEffect(() => {
     editor?.setOptions({ editable });
+    editor?.commands.setContent(initialContent);
+
+    if(!removeHTMLTags(initialContent).length && !editable ) {
+      editor?.commands.setContent(`<p>${placeholder}</p>`)
+    }
 
     if(editable){
       editor?.commands.focus()
     }
-
-  }, [editable, editor]);
+  }, [editable, editor, initialContent]);
 
   const isEditor = useMemo(() => editor !== null, [editor])
 
-  if (!isEditor) {
-    return null
-  }
-
+  if (!isEditor) return null
 
   return (
     <div ref={editorRef} className="w-full">
-
       {editor && (
         <FloatingMenu editor={editor}
-          className="bg-zinc-700 p-1 border border-zinc-600 rounded-lg shadow-xl shadow-black/20 overflow-hidden flex flex-col"
+          className="bg-card p-1 rounded-lg shadow-custom overflow-hidden flex flex-col hover:bg-muted/40"
           shouldShow={({ state }) => {
             const { $from } = state.selection
             const currentLineText = $from.nodeBefore?.textContent
@@ -87,7 +121,7 @@ export function Editor({ initialContent } : EditorProps ) {
           }}
         >
           <button
-            className="p-1 flex items-center gap-2 font-medium min-w-[280px] hover:bg-zinc-600"
+            className="p-1 flex items-center gap-2 font-medium min-w-[280px] hover:bg-muted/40 hover:text-primary rounded-lg"
             onClick={(ev) => {
               const { tr } = editor.state;
               const { from } = editor.state.selection;
@@ -110,7 +144,7 @@ export function Editor({ initialContent } : EditorProps ) {
 
       <BubbleMenuComponent editor={editor} />
 
-      <EditorContent editor={editor} className="w-full p-2 max-w-[700px] prose prose-invert prose-emerald prose-p:m-1 text-muted-foreground"/>
+      <EditorContent editor={editor} className="w-full p-2 max-w-[700px] prose  prose-emerald prose-p:m-1 text-muted-foreground truncate"/>
     </div>
   )
 }
